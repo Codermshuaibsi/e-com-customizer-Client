400a27a5ebaf501b0a0328b28400ff4f733c529c
Amanpal123450 <amanpal600@gmail.com>
2025-07-30 21:45:06 +0530

    Initial commit or update

---
+++ src/app/login/page.js
@@ -0,0 +1,19 @@
+"use client";
+
+import React from "react";
+import Login from "./login_page/page";
+
+
+
+function signup_AllSection() {
+  return (
+    <>
+      <div className=" mx-auto max-w-[1720px]">
+         
+       <Login />
+      </div>
+    </>
+  );
+}
+
+export default signup_AllSection;
