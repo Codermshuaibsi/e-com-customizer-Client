1fd3a09643c1261f012e2f10f4b373a23d386ccd
Mohd Shuaib <heyshuaib43@gmail.com>
2025-08-04 14:12:33 +0530

    new changes

--- src/app/new/FeaturedProduct/page.js
+++ src/app/new/FeaturedProduct/page.js
@@ -8,22 +8,6 @@ import Link from "next/link";
 import Toastify from "toastify-js";
 import "toastify-js/src/toastify.css";
 
-export const alert = (text, type = "success") => {
-  Toastify({
-    text: text,
-    duration: 3000,
-    close: true,
-    gravity: "top",
-    position: "right",
-    backgroundColor:
-      type === "success"
-        ? "linear-gradient(to right, #00b09b, #96c93d)"
-        : type === "error"
-        ? "linear-gradient(to right, #ff5f6d, #ffc371)"
-        : "#333",
-  }).alert();
-};
-
 export default function FeaturePro() {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
